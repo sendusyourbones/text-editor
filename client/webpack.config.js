15f4bbbf5575ae6fc738c2aa17c89086ab808188
sendusyourbones <beckee.roos@gmail.com>
2023-06-20 21:38:25 -0700

    Add larger icon size to fix install functionality

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,12 +31,12 @@ module.exports = () => {
         description: 'Write and save your text',
         background_color: '#7eb4e2',
         theme_color: '#7eb4e2',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('./src/images/logo.png'),
-            sizes: [96],
+            sizes: [96, 144],
             destination: path.join('assets', 'icons'),
           },
         ],
